ea4adc9276fe325d9bc23d7a02eedf5038168552
ritcabuchi <ridwanritcabuchi@gmail.com>
2018-03-03 15:32:48 +0700

    Simple C

---
+++ Davinchi.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    char sen[20],new[20];
+    int i;
+	printf("Enter word:");
+	scanf("%s",&sen);
+	for(i=0;i<=100;i++){
+		printf("\n");
+	}
+	scanf("%s",&new);
+	return 0;
+}
